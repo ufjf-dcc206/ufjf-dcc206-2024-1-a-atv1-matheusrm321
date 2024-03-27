6b6910bc770e11a39cf60b79c0aa4a65dc83f15f
Matheus Ribeiro <matheusribeiro.menezes@estudante.ufjf.br>
2024-03-26 22:13:23 -0300

    adiciona proximo

--- proximo.test.js
+++ proximo.test.js
@@ -1,4 +1,5 @@
 import { test, expect} from 'vitest';
+import { proximo } from "./proximo";
 
 test('proximo(0) retorna 1', ()=>{
 
@@ -13,6 +14,11 @@ test('proximo(1) retorna 3', ()=>{
 test('proximo(2) retorna 5', ()=>{
 
     expect(proximo(2)).toBe(5);
+});
+
+test('proximo(3) retorna 4', ()=>{
+
+    expect(proximo(3)).toBe(4);
 });
 
 test('proximo(4) retorna 6', ()=>{
